811de27a9e3da0a76ff7bfb09ca4b23948d6ceb7
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-13 23:05:30 -0400

    better comments and changed the basic structure for the reload method, line 52 - 55

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,14 +19,14 @@ class FileStorage:
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+            dict_for_json = {}
+            dict_for_json.update(FileStorage.__objects)
+            for key, obj in dict_for_json.items():
+                dict_for_json[key] = obj.to_dict()
+            json.dump(dict_for_json, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads storage dictionary from file.json"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -35,16 +35,23 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        classes_dict = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'Place': Place,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Review': Review
+            }
+
         try:
-            temp = {}
+            dict_from_json = {}
             with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                dict_from_json = json.load(f)
+                for key, obj_dictionary in dict_from_json.items():
+                    class_name = obj_dictionary["__class__"]  # Get class name
+                    class_to_call = classes_dict[class_name]
+                    self.__objects[key] = class_to_call(**obj_dictionary)
         except FileNotFoundError:
             pass
