199d8297a5c2f4558c53c81989aef1edca41b850
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-18 14:25:23 -0400

    task 8

--- models/user.py
+++ models/user.py
@@ -3,11 +3,19 @@
 
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class User(BaseModel, Base):
     __tablename__ = 'users'
 
+    # Columns
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    # Relationships
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship('Place', backref='user', cascade='all, delete')
+
