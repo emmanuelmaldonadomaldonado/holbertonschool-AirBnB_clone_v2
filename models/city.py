19b868a55d7c0378917a722062dca70ef521b97b
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-18 16:01:27 -0400

    Task 9

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@
 """BACKUP VERSION"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -10,3 +12,7 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # Relationships
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship('Place', backref='cities', cascade='all, delete')
