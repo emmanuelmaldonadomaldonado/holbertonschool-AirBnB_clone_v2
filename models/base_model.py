a6cd975ecbe530565e3d5fe8c30ce3e778f42bc0
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-18 12:55:36 -0400

    change for else in init of basemodel, if not revert

--- models/base_model.py
+++ models/base_model.py
@@ -38,6 +38,8 @@ class BaseModel:
             self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             if 'created_at' in kwargs:
                 kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                           '%Y-%m-%dT%H:%M:%S.%f')
