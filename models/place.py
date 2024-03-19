5263f4caa86f9721751a650115d01a21d6b3801b
Emmanuel Maldonado <146750306+emmanuelmaldonadomaldonado@users.noreply.github.com>
2024-03-18 22:56:22 -0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'))
-    user_id = Column(String(60), ForeignKey('users.id'))
-    name = Column(String(60), nullable=False)
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
@@ -17,4 +20,12 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+
+    user = relationship("User", back_populates="places")
+    city = relationship("City", back_populates="places")
+
+    place_amenities = Table('place_amenity', Base.metadata,
+                            Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
