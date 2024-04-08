704dcc3ad2067e60c36e76807584ad81835af917
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-08 14:36:24 -0400

    flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,6 @@ class FileStorage:
         if obj is not None:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del FileStorage.__objects[key]
+
+    def close(self):
+        self.reload()
