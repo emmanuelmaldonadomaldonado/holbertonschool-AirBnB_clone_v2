e6ed61da2b1fb724c352e093846f8983ccd65d93
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-12 10:59:11 -0400

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,11 +5,48 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
+import sys
+sys.path.append("/home/emmanuel/holbertonschool-AirBnB_clone_v2")
 from models import storage
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_1():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_2(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_3(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hello_4(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def hello_5(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def hello_6(n):
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 @app.route("/states_list", strict_slashes=False)
