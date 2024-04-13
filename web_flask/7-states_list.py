8f4ec0c54b5edc166e4f58c7f47f75e3e73a39f6
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-12 20:45:14 -0400

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,64 +1,59 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Script that starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
-import sys
-sys.path.append("/home/emmanuel/holbertonschool-AirBnB_clone_v2")
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+import sys
+sys.path.append('/home/osvaldo_jose1/holbertonschool-AirBnB_clone_v2')
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_1():
-    return 'HBNB'
+def hbnb():
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_2(text):
-    return 'C ' + text.replace('_', ' ')
+def c(text):
+    return "C " + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_3(text='is cool'):
-    return 'Python ' + text.replace('_', ' ')
+def python(text='is cool'):
+    return "Python " + text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_4(n):
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_5(n):
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", number=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def hello_6(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Render state_list.html"""
+    states = storage.all(State)
+    states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
